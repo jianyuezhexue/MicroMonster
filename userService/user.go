7b9b906933e8ce1fdf0ab83ce9ca8abd4b2c9983
490513975 <490513975@qq.com>
2023-04-02 15:09:26 +0800

    结构梳理

--- userService/user.go
+++ userService/user.go
@@ -1,12 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"iRet":  0,
+			"sMsg":  "success",
+			"jData": "pong",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
